d6885ceb686990f6b793bab14ca2241645877c67
Victor Santos <victoorsaantos16@gmail.com>
2024-09-12 13:44:02 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let itens = container.querySelectorAll('.list .item')
+let indicator = document.querySelector('.indicators')
+let dots = indicator.querySelectorAll('ul li')
+
+let active = 0
+let firstPosition = 0
+let lastPosition = itens.length - 1
+
+
+nextButton.onclick = () => {
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
+
+    active = active + 1 > lastPosition ? 0 : active + 1
+    itens[active].classList.add('active')
+}
+
+prevButton.onclick = () => {
+    console.log("Botão prev")
+}
